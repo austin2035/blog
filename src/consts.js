742843ae406213fabf9986efd4a52ef099707521
Austin <lookcos@gmail.com>
2024-06-04 16:55:40 +0800

    Update tags in archive page and site description in consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `驭风笔记`;
-export const SITE_DESCRIPTION = '驭风笔记，分享我读过的书、学过的技术、遇见的人等。';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_DESCRIPTION = '驭风笔记， 分享科技、AI、编码、赚钱相关的见闻和知识';
+export const SITE_EMAIL = 'no@gmail.com'
 export const SITE_NAME = 'yufengbiji.com';
 export const SITE_URL = "https://yufengbiji.com";
